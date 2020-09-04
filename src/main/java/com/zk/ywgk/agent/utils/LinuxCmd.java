048423fe622f8d3ed33ce35f69b19f0f61b26045
Modest-Honest <741905218@qq.com>
2020-09-04 16:46:07 +0800

    2020/9/4 add method:LinuxRead()

--- src/main/java/com/zk/ywgk/agent/utils/LinuxCmd.java
+++ src/main/java/com/zk/ywgk/agent/utils/LinuxCmd.java
@@ -4,11 +4,12 @@ import java.io.*;
 
 
 public class LinuxCmd {
-	Process process = null;
-	BufferedReader buf = null;
-	String result = "";
+
 	
 	public String exec(String cmd) {
+		Process process = null;
+		BufferedReader buf = null;
+		String result = "";
 		try {
 			process = Runtime.getRuntime().exec(new String[] {"sh","-c",cmd});
 			buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -25,6 +26,26 @@ public class LinuxCmd {
 		}
 		return result;
 
+		
+	}
+	
+	public String readFile(File file)  {
+		String result = "";
+		String line = null;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+			while ((line = br.readLine())!=null) {
+				result = result + line + " ";
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 		
 	}
 	
